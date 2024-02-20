2bc57b5cf7099c1e12113f4666c6c9d714b9e5c4
Bruno <158314045+Brun0H28@users.noreply.github.com>
2024-02-20 14:37:21 -0300

    mudança

--- atividade02.js
+++ atividade02.js
@@ -1,3 +1,4 @@
+//ex01
 var nome = "Pablo";
 var nascimento = 1969;
 console.log(nome, "nasceu em", nascimento);
@@ -6,30 +7,42 @@ var atual = idade + -nascimento;
 console.log("Pablo tem", atual, "anos");
 console.log("-----------------");
 
+//ex02
+
 var comprimento = 12;
 var largura = 6;
 var area = largura * comprimento;
 console.log("A área é de", area, "cm²");
 console.log("-----------------");
+
+//ex03
+
 var altura = 5;
 var volume = comprimento * largura * altura;
 console.log("O volume é de ", volume, "cm³");
 console.log("-----------------");
+
+//ex04
+
 var a = 2;
 var b = 3;
 var c = -1;
 var delta = b ** 2 - 4 * a * c;
 console.log("O delta tem valor de", delta);
 console.log("-----------------");
+
+//ex05
+
 var compra = 450
 var desconto = compra / 100 * 3
 var valorFinal = compra - desconto
 console.log("o valor do desconto é R$", desconto, "|O valor final da compra é de", valorFinal, "R$")
 console.log("-----------------");
+
+//ex06
+
 var velMedia = 95
 var Tempo = 3.5
 var distancia = velMedia * Tempo
 console.log("A distancia percorrida foi de", distancia, "KM")
 console.log("-----------------");
-
-
